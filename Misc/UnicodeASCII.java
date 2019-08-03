64a2d1ab4cbff57a3a89b37fca27a36b16b24f57
Alexander Amundson <alex.amundson987@gmail.com>
2019-08-03 09:40:03 -0400

    ASCII-Unicode-Character conversions

--- Misc/UnicodeASCII.java
+++ Misc/UnicodeASCII.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class ASCII_Numeric_Conversion {
+public class UnicodeASCII {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int choice = getUserChoice(scanner);
@@ -7,11 +7,12 @@ public class ASCII_Numeric_Conversion {
 		boolean tryAgain = true;
 		while (tryAgain) {
 			switch (choice) {
-			case 0: tryAgain = false;  closeProgram(scanner); 
+			case 0: tryAgain = false;  closeProgram(scanner);  
 			case 1: ASCII_to_character(scanner);
-			           getUserChoice(scanner);
+			           choice = getUserChoice(scanner);
 			           break;
-			case 2: ;
+			case 2: characterToUnicode(scanner); 
+			           choice = getUserChoice(scanner);
 			           break;
 			case 3: ;
 					   break;
@@ -24,18 +25,27 @@ public class ASCII_Numeric_Conversion {
 	
     //Returns which option the user chooses
 	private static int getUserChoice(Scanner scanner) {
-		StringBuilder options = new StringBuilder("\n------------Options------------ ");
+		StringBuilder options = new StringBuilder("\n\n------------Options------------ ");
 		options.append("\n0 -- to exit program\n1-- Convert ASCII number to char value");
+		options.append("\n2-- Convert character to Unicode value");
 		System.out.println(options);
 		return scanner.nextInt();
 	}
    
 	//Prints the character that corresponds to the given ASCII number
 	private static void ASCII_to_character(Scanner scan) {
-		System.out.println("\nEnter an integer between 33 and 126");
 		int ascii = Integer.parseInt( scan.next() );
+		System.out.println("\nEnter an integer between 33 and 126");
 		String output = Character.toString((char) ascii);
 		System.out.printf("The ASCII value %d corresponds to the character: %8s", ascii, output);
+	}
+	
+	//Prints the Unicode corresponding to character entered by user
+	private static void characterToUnicode(Scanner scan) {
+		System.out.println("\nEnter a single character");
+		char input = scan.next().charAt(0);
+		System.out.printf("The character '%c' corresponds to the Unicode value: %5d", input, (int)input );
+		
 	}
 	
 	//Terminates the program
